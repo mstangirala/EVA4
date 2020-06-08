ab2eb0d6b14a2565b42c96ba7bb715e10ce5aa19
mstangirala <55681983+mstangirala@users.noreply.github.com>
2020-06-08 18:46:45 +0530

    Add files via upload

---
+++ Session 8/Albumentations.py
@@ -0,0 +1,26 @@
+from __future__ import print_function
+import torch
+import torch.nn as nn
+import torch.nn.functional as F
+import torch.optim as optim
+from torchvision import datasets, transforms
+import matplotlib.pyplot as plt
+import os
+
+# Train Phase transformations
+train_transforms = transforms.Compose([
+                                      #  transforms.Resize((28, 28)),
+                                      #  transforms.ColorJitter(brightness=0.10, contrast=0.1, saturation=0.10, hue=0.1),
+                                       transforms.RandomRotation((-7.0, 7.0), fill=(1,)),
+                                       transforms.ToTensor(),
+                                       transforms.Normalize((0.1307,), (0.3081,)) # The mean and std have to be sequences (e.g., tuples), therefore you should add a comma after the values. 
+                                       # Note the difference between (0.1307) and (0.1307,)
+                                       ])
+
+# Test Phase transformations
+test_transforms = transforms.Compose([
+                                      #  transforms.Resize((28, 28)),
+                                      #  transforms.ColorJitter(brightness=0.10, contrast=0.1, saturation=0.10, hue=0.1),
+                                       transforms.ToTensor(),
+                                       transforms.Normalize((0.1307,), (0.3081,))
+                                       ])
